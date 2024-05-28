343a32c1b06b1c718eab56ff2c98e8e08dc2e0b3
JorgeHernandez-Hexaware <112416092+JorgeHernandez-Hexaware@users.noreply.github.com>
2024-05-28 16:26:37 -0600

    Update OrangeHMRT
    Implement try/catch to validate driver

--- Practice/OrangeHMRT.java
+++ Practice/OrangeHMRT.java
@@ -52,98 +52,107 @@ public class OrangeHMRT {
 		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
-		//Navigate to Page
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		takeScreenshot("Before_typing_credentials", driver);
 		
-		//Type credentials to login
-		WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(usernameF));
-		highlightElement(usernameField, driver);
-		usernameField.sendKeys("Admin");
-		
-		WebElement passwordField = driver.findElement(passwordF);
-		highlightElement(passwordField, driver);
-		passwordField.sendKeys("admin123");
-		
-		WebElement loginButton  = driver.findElement(loginBtn);
-		
-		takeScreenshot("After_typing_credentials", driver);
-		highlightElement(loginButton, driver);
-		loginButton.click();
-		
-		//Go to time
-		WebElement timeButton = wait.until(ExpectedConditions.visibilityOfElementLocated(timeBtn));
-		timeButton.click();
-				
-		WebElement inptEmpName = wait.until(ExpectedConditions.visibilityOfElementLocated(inputEmpName));
-		inptEmpName.click();
-		inptEmpName.sendKeys("James  Butler");
-		
-		WebElement divD = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
-		Boolean divDrp = wait.until(ExpectedConditions.textToBe(divDrop, "James Butler"));
-		if(divDrp) {
-			inptEmpName.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
-		}
+		try {
+			//Navigate to Page
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+			takeScreenshot("Before_typing_credentials", driver);
 			
-		WebElement viewButtonEmpName = wait.until(ExpectedConditions.visibilityOfElementLocated(viewBtnEmpName));
-		viewButtonEmpName.click();
-		
-		
-		WebElement btnEditTS = wait.until(ExpectedConditions.visibilityOfElementLocated(btnEditTimeSheet));
-		btnEditTS.click();
-		
-		WebElement btnTrashIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(BtnTrash));
-		if(btnTrashIcon.isDisplayed()) {
-			btnTrashIcon.click();
+			//Type credentials to login
+			WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(usernameF));
+			highlightElement(usernameField, driver);
+			usernameField.sendKeys("Admin");
+			
+			WebElement passwordField = driver.findElement(passwordF);
+			highlightElement(passwordField, driver);
+			passwordField.sendKeys("admin123");
+			
+			WebElement loginButton  = driver.findElement(loginBtn);
+			
+			takeScreenshot("After_typing_credentials", driver);
+			highlightElement(loginButton, driver);
+			loginButton.click();
+			
+			//Go to time
+			WebElement timeButton = wait.until(ExpectedConditions.visibilityOfElementLocated(timeBtn));
+			timeButton.click();
+					
+			WebElement inptEmpName = wait.until(ExpectedConditions.visibilityOfElementLocated(inputEmpName));
+			inptEmpName.click();
+			inptEmpName.sendKeys("James  Butler");
+			
+			WebElement divD = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
+			Boolean divDrp = wait.until(ExpectedConditions.textToBe(divDrop, "James Butler"));
+			if(divDrp) {
+				inptEmpName.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+			}
+				
+			WebElement viewButtonEmpName = wait.until(ExpectedConditions.visibilityOfElementLocated(viewBtnEmpName));
+			viewButtonEmpName.click();
+			
+			
+			WebElement btnEditTS = wait.until(ExpectedConditions.visibilityOfElementLocated(btnEditTimeSheet));
+			btnEditTS.click();
+			
+			WebElement btnTrashIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(BtnTrash));
+			if(btnTrashIcon.isDisplayed()) {
+				btnTrashIcon.click();
+			}
+			
+			WebElement inpPEmployee = wait.until(ExpectedConditions.visibilityOfElementLocated(inputProject));
+			inpPEmployee.click();
+			inpPEmployee.sendKeys("ACME");
+			WebElement divDProject = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
+			highlightElement(divDProject, driver);
+			Boolean divDrpPj = wait.until(ExpectedConditions.textToBe(divDrop, "ACME Ltd - ACME Ltd"));
+			if(divDrpPj) {
+				inpPEmployee.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+			}
+			
+			WebElement divActivity = driver.findElement(divAct);
+			divActivity.click();
+			WebElement actDiv = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
+			
+			Thread.sleep(2000);
+			
+			if(actDiv.isDisplayed()) {
+				divActivity.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
+			}
+			
+			 List<WebElement> inpHours = driver.findElements(inpHour);
+			 inpHours.get(1).click();
+			 inpHours.get(1).sendKeys("10");
+			 inpHours.get(2).click();
+			 inpHours.get(2).sendKeys("10");
+			 inpHours.get(3).click();
+			 inpHours.get(3).sendKeys("10");
+			 inpHours.get(4).click();
+			 inpHours.get(4).sendKeys("10");
+			 inpHours.get(5).click();
+			 inpHours.get(5).sendKeys("10");
+			 inpHours.get(6).click();
+			 inpHours.get(6).sendKeys("10");
+			 inpHours.get(7).click();
+			 inpHours.get(7).sendKeys("10");
+			 
+			 WebElement buttonSave = driver.findElement(btnSave);
+			 buttonSave.click();
+			 
+			 Thread.sleep(3000);
+			 takeScreenshot("Validating_create_timesheet", driver);
+			 
+			 takeScreenshot("Validating_total_hours", driver);
+			 
+			
+			//Close driver
+			driver.quit();
+			
+		} catch (NullPointerException e) {
+			System.out.println("Error: " + e);
+			driver.quit();
 		}
 		
-		WebElement inpPEmployee = wait.until(ExpectedConditions.visibilityOfElementLocated(inputProject));
-		inpPEmployee.click();
-		inpPEmployee.sendKeys("ACME");
-		WebElement divDProject = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
-		highlightElement(divDProject, driver);
-		Boolean divDrpPj = wait.until(ExpectedConditions.textToBe(divDrop, "ACME Ltd - ACME Ltd"));
-		if(divDrpPj) {
-			inpPEmployee.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
-		}
 		
-		WebElement divActivity = driver.findElement(divAct);
-		divActivity.click();
-		WebElement actDiv = wait.until(ExpectedConditions.visibilityOfElementLocated(divDrop));
-		
-		Thread.sleep(2000);
-		
-		if(actDiv.isDisplayed()) {
-			divActivity.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
-		}
-		
-		 List<WebElement> inpHours = driver.findElements(inpHour);
-		 inpHours.get(1).click();
-		 inpHours.get(1).sendKeys("10");
-		 inpHours.get(2).click();
-		 inpHours.get(2).sendKeys("10");
-		 inpHours.get(3).click();
-		 inpHours.get(3).sendKeys("10");
-		 inpHours.get(4).click();
-		 inpHours.get(4).sendKeys("10");
-		 inpHours.get(5).click();
-		 inpHours.get(5).sendKeys("10");
-		 inpHours.get(6).click();
-		 inpHours.get(6).sendKeys("10");
-		 inpHours.get(7).click();
-		 inpHours.get(7).sendKeys("10");
-		 
-		 WebElement buttonSave = driver.findElement(btnSave);
-		 buttonSave.click();
-		 
-		 Thread.sleep(3000);
-		 takeScreenshot("Validating_create_timesheet", driver);
-		 
-		 takeScreenshot("Validating_total_hours", driver);
-		 
-		
-		//Close driver
-		driver.quit();
 	}
 	
 	private static void takeScreenshot(String name, WebDriver driver) throws IOException{
